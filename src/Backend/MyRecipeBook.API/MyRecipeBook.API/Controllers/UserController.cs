b7a7701822cb693e50de99977c11b89655f1bfca
Douglas dos Santos Oliveira <92030167+tilo1306@users.noreply.github.com>
2024-05-19 21:30:38 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/MyRecipeBook.API/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterUserJson request)
+    {
+        return Created();
+    }
+}
